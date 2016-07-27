245c7ec3ca0e86585c326e5a1a2d8335aed2e2da
Timothy Ellsworth Bowers <tbowers7@gmail.com>
2016-07-27 13:52:26 -0700

    Working, compiling, installing version of the library.

--- include/tpeb.h
+++ include/tpeb.h
@@ -1,3 +1,45 @@
+/* TPEB Library
+ * 
+ * A library of routines for astronomical use or wrappers around lower-level
+ * libraries for ease of use.
+ * 
+ * Timothy P. Ellsworth Bowers
+ * 
+ * FILE: tpeb.h
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#ifndef _TPEB_H
+#define _TPEB_H
+
+/* THIS FILE IS A COMPOSITE OF HEADERS FOR THE INDIVIDUAL SOURCE CODE FILES. */
+
+
+/******** astrom.h ********/
+/* Timothy Ellsworth Bowers
+   11 June 2009
+
+   Header file for astrometry-related functions needed for various
+   astronomical calculations.  All algorithms are from
+   Astronomical Algorithms by Jean Meeus, 2nd edition --
+   unless otherwise specified.
+
+*/
 /******** atime.h ********/
 /* Timothy Ellsworth Bowers
    11 October 2009
@@ -8,13 +50,105 @@
    unless otherwise specified.
 
 */
+/******** catalog.h ********/
+/* Timothy Ellsworth Bowers
+   11 June 2009
 
-#if !defined(ATIME_H)
+   Header file for catalog-related funtions needed for various
+   astronomical calculations.
 
-#define ATIME_H 1
+*/
+/******** coord.h ********/
+/* Timothy Ellsworth Bowers
+   11 June 2009
 
+   Header file for coordinate-related functions needed for various
+   astronomical calculations.
+
+*/
+/******** fileio.h ********/
+/* Timothy Ellsworth Bowers
+   18 September 2008
+
+   Header file that contains the various permutations of file opening,
+   and contains the proper error-handling, etc.  This is purely shorthand
+   because I am lazy.
+
+*/
+/******** fits_wrap.h ********/
+/* Timothy Ellsworth Bowers
+   21 January 2009
+
+   Header file for the fits_wrap.c source code.  CFITSIO is a low-level C
+   library for performing operations on FITS files.  The functions declared
+   here provide a high-level 'wrapper' functionality for common FITS I/O
+   tasks.  
+
+*/
+/******** imutil.h ********/
+/* Timothy Ellsworth Bowers
+   20 January 2010
+
+   Header file for the imutil.c source code.  Image utility routines.
+
+*/
+/******** photom.h ********/
+/* Timothy Ellsworth Bowers
+   9 July 2009
+
+   Header file for photometry-related funtions needed for various
+   astronomical calculations.
+
+*/
+/******** read_dat_files.h ********/
+/* Timothy Ellsworth Bowers
+   28 February 2009
+
+   Header file for the read_ncolumn routines, plus countlines().
+*/
+/******** strings.h ********/
+/* Timothy Ellsworth Bowers
+   9 October 2009
+
+   Header file to accompany the strings library routines.
+
+*/
+/******** window.h ********/
+/* Timothy Ellsworth Bowers
+   20 January 2010
+
+   Header definitions for various apodization window functions.
+*/
+
+
+/* Includes for external headers */
+#ifndef HAVE_MATH_H
+  #define HAVE_MATH_H
+  #include <math.h>
+#endif
+
+#ifndef HAVE_FITSIO_H
+  #define HAVE_FITSIO_H
+  #include <fitsio.h>
+#endif
+
+/* Define Constants */
 #define ATIME_LOCAL 45
 #define ATIME_GMT   46
+  /* c_type definitions for deg/dms routines */
+#define COORD_RA  360
+#define COORD_DEC 180
+#define COORD_LST 24
+  /* If 'Parallactic Angle'  */
+#define CATALOG_PA -500.
+#define STRINGS_LEN 256
+  /* Radian --> Deg & Deg --> Radian conversions */
+#define DEG2RAD M_PI/180.
+#define RAD2DEG 180./M_PI
+#define TWOPI   (M_PI*2.)
+#define OBSFILE "/d1/observing/Library/observatories.dat"
+
+
 
 /* Structure Definitions */
 typedef struct{
@@ -26,63 +160,6 @@ typedef struct{
   int min;
   double sec;
 } atime_time;
-
-
-/* Function Declarations */
-atime_time *atime_get_date_now(int clock_type);
-atime_time *atime_get_date(int clock_type, int prevday);
-double atime_jd(int year, int month, double day);
-double atime_jd_today();
-double atime_jd_now();
-double atime_get_lst(double longitude);
-double atime_lst(double longitude, double jd);
-char  *atime_datestring();
-
-#endif
-/******** photom.h ********/
-/* Timothy Ellsworth Bowers
-   9 July 2009
-
-   Header file for photometry-related funtions needed for various
-   astronomical calculations.
-
-*/
-
-#if !defined(PHOTOM_H)
-
-#define PHOTOM_H 1
-
-
-/* Function declarations */
-double photom_spect_countrate(double, double, double, double, double);
-
-#endif
-/******** strings.h ********/
-/* Timothy Ellsworth Bowers
-   9 October 2009
-
-   Header file to accompany the strings library routines.
-
-*/
-
-#define STRINGS_LEN 256
-
-int strings_getline(FILE *, char *, size_t *);
-/******** catalog.h ********/
-/* Timothy Ellsworth Bowers
-   11 June 2009
-
-   Header file for catalog-related funtions needed for various
-   astronomical calculations.
-
-*/
-
-#if !defined(CATALOG_H)
-
-#define CATALOG_H 1
-
-/* If 'Parallactic Angle'  */
-#define CATALOG_PA -500.
 
 /* Typedef catalog structures */
 
@@ -130,160 +207,6 @@ typedef struct {
   float  b_v;
 } catalog_bg;
 
-
-/* Function declarations */
-catalog_lib *catalog_read_lib(char *filename, int *n);
-catalog_mmt *catalog_read_mmt(char *filename, int *n);
-catalog_tui *catalog_read_tui(char *filename, int *n);
-
-#endif
-/******** read_dat_files.h ********/
-/* Timothy Ellsworth Bowers
-   28 February 2009
-
-   Header file for the read_ncolumn routines, plus countlines().
-*/
-
-#if !defined(READ_DAT_FILES_H)
-
-#define READ_DAT_FILES_H 1
-
-int countlines(FILE *fp);
-double *read_ncolumn(char *filename, int *N, int m);
-double *parse_array(double *total_array, int n_lines, int n_dim);
-
-#endif
-/******** fits_wrap.h ********/
-/* Timothy Ellsworth Bowers
-   21 January 2009
-
-   Header file for the fits_wrap.c source code.  CFITSIO is a low-level C
-   library for performing operations on FITS files.  The functions declared
-   here provide a high-level 'wrapper' functionality for common FITS I/O
-   tasks.  
-
-*/
-
-#if !defined(FITS_WRAP_H)
-
-#define FITS_WRAP_H 1
-
-fitsfile *fits_wrap_open_read(char *filename, int *status);
-fitsfile *fits_wrap_open_readwrite(char *filename, int *status);
-double **fits_wrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
-			      int data_type, int *status);
-void fits_wrap_write2file(char *fileout, char *copyhdr, double **array, 
-			  long subsize[2], int *status);
-
-
-#endif
-/******** coord.h ********/
-/* Timothy Ellsworth Bowers
-   11 June 2009
-
-   Header file for coordinate-related functions needed for various
-   astronomical calculations.
-
-*/
-
-#if !defined(COORD_H)
-
-#define COORD_H 1
-
-/* c_type definitions for deg/dms routines */
-#define COORD_RA  360
-#define COORD_DEC 180
-#define COORD_LST 24
-
-#include "astrom.h"
-
-/* Function declarations */
-astrom_coords  coord_parserd(char *);
-double         coord_dmstodeg(double dms[3], int c_type);
-void           coord_degtodms(double, char *, int c_type);
-
-#endif
-/******** window.h ********/
-/* Timothy Ellsworth Bowers
-   20 January 2010
-
-   Header definitions for various apodization window functions.
-*/
-
-#if !defined(WINDOW_H)
-
-#define WINDOW_H 1
-
-void window_andrew(double *array, int length, int n);
-void window_hann(double *array, int length);
-void window_hamming();
-
-#endif
-/******** imutil.h ********/
-/* Timothy Ellsworth Bowers
-   20 January 2010
-
-   Header file for the imutil.c source code.  Image utility routines.
-
-*/
-
-#if !defined(IMUTIL_H)
-
-#define IMUTIL_H 1
-
-double **imutil_alloc_2darray(long *);
-void     imutil_free_2darray(double **, long *);
-double  *imutil_2d_to_1d(double **, long *);
-double **imutil_get_subsection(double **, long *, long *, long *);
-void     imutil_transpose(double **, double **, int, int);
-
-#endif
-/******** fileio.h ********/
-/* Timothy Ellsworth Bowers
-   18 September 2008
-
-   Header file that contains the various permutations of file opening,
-   and contains the proper error-handling, etc.  This is purely shorthand
-   because I am lazy.
-
-*/
-
-#if !defined(FILEIO_H)
-
-#define FILEIO_H 1
-FILE *fileopenr(char *);
-FILE *fileopenw(char *);
-FILE *fileopenrb(char *);
-FILE *fileopenwb(char *);
-FILE *fileopenwa(char *);
-
-/* Create filename from existing root */
-void make_filename(char *,char *,char *,char *);
-
-#endif
-/******** astrom.h ********/
-/* Timothy Ellsworth Bowers
-   11 June 2009
-
-   Header file for astrometry-related functions needed for various
-   astronomical calculations.  All algorithms are from
-   Astronomical Algorithms by Jean Meeus, 2nd edition --
-   unless otherwise specified.
-
-*/
-
-#if !defined(ASTROM_H)
-
-#define ASTROM_H 1
-
-#include <math.h>
-
-/* Radian --> Deg & Deg --> Radian conversions */
-#define DEG2RAD M_PI/180.
-#define RAD2DEG 180./M_PI
-#define TWOPI   (M_PI*2.)
-#define OBSFILE "/d1/observing/Library/observatories.dat"
-
 /* Typedef structures for coordinates */
 
 // Celestial Coordinates
@@ -315,7 +238,62 @@ typedef struct {
 } astrom_rst;
 
 
-/* Function declarations */
+
+
+
+
+
+/* Function Declarations */
+atime_time *atime_get_date_now(int clock_type);
+atime_time *atime_get_date(int clock_type, int prevday);
+double atime_jd(int year, int month, double day);
+double atime_jd_today();
+double atime_jd_now();
+double atime_get_lst(double longitude);
+double atime_lst(double longitude, double jd);
+char  *atime_datestring();
+
+double photom_spect_countrate(double, double, double, double, double);
+
+int strings_getline(FILE *, char *, size_t *);
+
+catalog_lib *catalog_read_lib(char *filename, int *n);
+catalog_mmt *catalog_read_mmt(char *filename, int *n);
+catalog_tui *catalog_read_tui(char *filename, int *n);
+
+int countlines(FILE *fp);
+double *read_ncolumn(char *filename, int *N, int m);
+double *parse_array(double *total_array, int n_lines, int n_dim);
+
+fitsfile *fits_wrap_open_read(char *filename, int *status);
+fitsfile *fits_wrap_open_readwrite(char *filename, int *status);
+double **fits_wrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
+			      int data_type, int *status);
+void fits_wrap_write2file(char *fileout, char *copyhdr, double **array, 
+			  long subsize[2], int *status);
+
+astrom_coords  coord_parserd(char *);
+double         coord_dmstodeg(double dms[3], int c_type);
+void           coord_degtodms(double, char *, int c_type);
+
+void window_andrew(double *array, int length, int n);
+void window_hann(double *array, int length);
+void window_hamming();
+
+double **imutil_alloc_2darray(long *);
+void     imutil_free_2darray(double **, long *);
+double  *imutil_2d_to_1d(double **, long *);
+double **imutil_get_subsection(double **, long *, long *, long *);
+void     imutil_transpose(double **, double **, int, int);
+
+FILE *fileopenr(char *);
+FILE *fileopenw(char *);
+FILE *fileopenrb(char *);
+FILE *fileopenwb(char *);
+FILE *fileopenwa(char *);
+
+void make_filename(char *,char *,char *,char *);
+
 double astrom_ang_sep(const astrom_coords *, const astrom_coords *);
 double astrom_ang_sep2(const astrom_coords *, const astrom_coords *);
 double astrom_rel_pos_ang(const astrom_coords *, const astrom_coords *);
@@ -328,4 +306,5 @@ astrom_rst      *astrom_get_rst(astrom_coords *,astrom_location *,
 astrom_location *astrom_read_observatories(char *, int *);
 astrom_location  astrom_get_location(char *);
 
-#endif
+
+#endif  /* _TPEB_H */
