d623220fb19018488acc0c2377349bd16434df66
Timothy Ellsworth Bowers <tbowers7@gmail.com>
2016-07-30 14:39:37 -0700

    Update fits_wrap -> fitswrap, autotools.

--- include/tpeb.h
+++ include/tpeb.h
@@ -75,11 +75,11 @@
    because I am lazy.
 
 */
-/******** fits_wrap.h ********/
+/******** fitswrap.h ********/
 /* Timothy Ellsworth Bowers
    21 January 2009
 
-   Header file for the fits_wrap.c source code.  CFITSIO is a low-level C
+   Header file for the fitswrap.c source code.  CFITSIO is a low-level C
    library for performing operations on FITS files.  The functions declared
    here provide a high-level 'wrapper' functionality for common FITS I/O
    tasks.  
@@ -265,12 +265,13 @@ int countlines(FILE *fp);
 double *read_ncolumn(char *filename, int *N, int m);
 double *parse_array(double *total_array, int n_lines, int n_dim);
 
-fitsfile *fits_wrap_open_read(char *filename, int *status);
-fitsfile *fits_wrap_open_readwrite(char *filename, int *status);
-double **fits_wrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
+fitsfile *fitswrap_open_read(char *filename, int *status);
+fitsfile *fitswrap_open_readwrite(char *filename, int *status);
+double **fitswrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
 			      int data_type, int *status);
-void fits_wrap_write2file(char *fileout, char *copyhdr, double **array, 
+void fitswrap_write2file(char *fileout, char *copyhdr, double **array, 
 			  long subsize[2], int *status);
+void fitswrap_catcherror(int *status);
 
 astrom_coords  coord_parserd(char *);
 double         coord_dmstodeg(double dms[3], int c_type);
--- src/fitswrap.c
+++ src/fitswrap.c
@@ -1,4 +1,4 @@
-/******** fits_wrap.c ********/
+/******** fitswrap.c ********/
 /* Timothy Ellsworth Bowers
    24 March 2009
 
@@ -14,10 +14,10 @@
 
    This source file contains the following routines:
 
-   fits_wrap_open_read();            Open FITS file for reading ONLY
-   fits_wrap_open_readwrite();       Open FITS file for reading and writing
-   fits_wrap_read2array();           Reads FITS (subsection) into an array
-   fits_wrap_write2file();           Writes array to FITS file
+   fitswrap_open_read();            Open FITS file for reading ONLY
+   fitswrap_open_readwrite();       Open FITS file for reading and writing
+   fitswrap_read2array();           Reads FITS (subsection) into an array
+   fitswrap_write2file();           Writes array to FITS file
 
 */
 
@@ -25,16 +25,15 @@
 #include <unistd.h>
 #include <time.h>               // To update FITS headers
 
-#include <tpeb.h>
+#include <tpeb.h>               // TPEB Library Headers
 
 /* Routine for opening FITS file READONLY with error checking */
-fitsfile *fits_wrap_open_read(char *filename, int *status){
+fitsfile *fitswrap_open_read(char *filename, int *status){
   
   fitsfile *fitsfp;
   
   if(fits_open_file(&fitsfp, filename, READONLY, status)){
-    fits_report_error(stderr,*status);
-    exit(1);
+    fitswrap_catcherror(status);    // Send pointer not value
   }
   
   return fitsfp;
@@ -42,13 +41,12 @@ fitsfile *fits_wrap_open_read(char *filename, int *status){
 
 
 /* Routine for opening FITS file READONLY with error checking */
-fitsfile *fits_wrap_open_readwrite(char *filename, int *status){
+fitsfile *fitswrap_open_readwrite(char *filename, int *status){
   
   fitsfile *fitsfp;
   
   if(fits_open_file(&fitsfp, filename, READWRITE, status)){
-    fits_report_error(stderr,*status);
-    exit(1);
+    fitswrap_catcherror(status);    // Send pointer not value
   }
   
   return fitsfp;
@@ -57,7 +55,7 @@ fitsfile *fits_wrap_open_readwrite(char *filename, int *status){
 
 /* Routine for reading FITS into array, starting at point, and w/ size */
 /* This version assumes an open FITS file, and accepts the fitsfile pointer */
-double **fits_wrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
+double **fitswrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
 			      int data_type, int *status){
   
   /* Variable declarations & Initilaztion */
@@ -76,8 +74,7 @@ double **fits_wrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
   /* ERROR CHECKING & CONVERTING NOTATION */
     /* Get image file parameters */
     if(fits_get_img_param(fitsfp, 2, &bitpix, &naxis, naxes, status)){
-      fits_report_error(stderr,*status);
-      exit(1);
+      fitswrap_catcherror(status);    // Send pointer not value
     }    
     /* Error catching -- number of axes */
     if(naxis != 2){
@@ -123,7 +120,7 @@ double **fits_wrap_read2array(fitsfile *fitsfp, long xystart[2], long xysize[2],
 
 
 /* Function to write array to FITS file, and copy header info from other file */
-void fits_wrap_write2file(char *fileout, char *copyhdr, double **array, 
+void fitswrap_write2file(char *fileout, char *copyhdr, double **array, 
 			  long write_size[2], int *status){
   
   /* Variable Declarations & Initializations */
@@ -136,17 +133,16 @@ void fits_wrap_write2file(char *fileout, char *copyhdr, double **array,
   *status = 0;
 
   /* Open file from which header will be copied */
-  hdrfp = fits_wrap_open_read(copyhdr, status);
+  hdrfp = fitswrap_open_read(copyhdr, status);
 
   /* Check for output file -- create & open for write */
   if(access(fileout,F_OK) == 0)          // Check if output file exists
     remove(fileout);                     // If yes, remove it
   if(fits_create_file(&fitsfp, fileout, status)){
-    fits_report_error(stderr,*status);
-    exit(1);
+    fitswrap_catcherror(status);    // Send pointer not value
   }
 
-  mod_comm = "Modified by fits_wrap routine, TPEB.";
+  mod_comm = "Modified by fitswrap routine, TPEB.";
   /* Copy header from copyhdr FITS file. */
   fits_copy_header(hdrfp, fitsfp, status);  // Copy header from input FITS
   fits_get_img_param(hdrfp, 2 ,&bitpix, &naxis, naxes, status);
@@ -201,7 +197,24 @@ void fits_wrap_write2file(char *fileout, char *copyhdr, double **array,
   
   /* Report any CFITSIO errors to stderr */
   if(!*status)
-    fits_report_error(stderr,*status);
+    fitswrap_catcherror(status);    // Send pointer not value
+  
+  return;
+}
+
+
+/* Function to catch errors thrown by CFITSIO */
+void fitswrap_catcherror(int *status){
+  fits_report_error(stderr,*status);
+  
+  
+  /* Add error-catching code here */
+  // CASE statement using ERROR codes from fitsio.h
+  printf("We need to add some error-catching code here...\n");
+  
+  exit(1);
+  
+  
   
   return;
 }
